a159b55e017409353463c6a744de2f0d5da8126d
VeeBui <veeeebui@gmail.com>
2024-05-06 21:47:56 +1000

    Part 2 - Flask, complete

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,5 +1,13 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
@@ -7,26 +15,210 @@
 #################################################
 
 # Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # Declare a Base using `automap_base()`
+Base = automap_base()
 
 # Use the Base class to reflect the database tables
+Base.prepare(autoload_with=engine)
 
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
-
-# Create a session
 
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+
+############################################
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+
+    # Find the most recent date in the data set.
+    recent_date = session.query(Measurement.date).\
+        order_by(Measurement.date.desc()).\
+        first()[0]
+    
+    # Calculate the date one year from the last date in data set.
+    date_12months = dt.datetime.strptime(recent_date,"%Y-%m-%d") - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    prcp_12months = session.query(Measurement.date, Measurement.prcp).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) > date_12months).all()
+
+
+    session.close()
+
+    # Create dictionary
+    result = []
+    for date, prcp in prcp_12months:
+        prcp_dict = {}
+        # Student note: Honestly, this doesn't make sense to me as each date isn't unique
+        prcp_dict[date] = prcp
+        result.append(prcp_dict)
+
+    return jsonify(result)
+
+
+
+############################################
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    # Query stations
+    station_list = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+
+    session.close()
+
+    # Create dictionary
+    result = []
+    for station, name, lat, lon, elev in station_list:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["lat"] = lat
+        station_dict["lon"] = lon
+        station_dict["elev"] = elev
+        result.append(station_dict)
+
+    return jsonify(result)
+
+
+
+############################################
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    # Find the most recent date in the data set.
+    recent_date = session.query(Measurement.date).\
+        order_by(Measurement.date.desc()).\
+        first()[0]
+    
+    # Calculate the date one year from the last date in data set.
+    date_12months = dt.datetime.strptime(recent_date,"%Y-%m-%d") - dt.timedelta(days=365)
+
+    # Get most active station
+    station_counts = session.query(Measurement.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).\
+        all()
+
+    act_station = station_counts[0][0]
+
+    # Get date and tobs from most active station
+    act_station_12months = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == act_station).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) > date_12months).\
+        all()
+
+    session.close()
+
+    # Create dictionary
+    result = []
+    for date, tobs in act_station_12months:
+        ActStat_dict = {}
+        ActStat_dict[date] = tobs
+        result.append(ActStat_dict)
+
+    return jsonify(result)
+
+
+
+############################################
+@app.route("/api/v1.0/<start>")
+def tempByStart(start):
+    session = Session(engine)
+
+    values = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) >= dt.datetime.strptime(start,"%Y-%m-%d")).\
+        all()
+
+    session.close()
+
+    # Create dictionary
+    result = []
+    for mn, avg, mx in values:
+        tobs_stats_dict = {}
+        tobs_stats_dict["min"] = mn
+        tobs_stats_dict["avg"] = avg
+        tobs_stats_dict["max"] = mx
+        result.append(tobs_stats_dict)
+
+    return jsonify(result)
+
+
+
+############################################
+@app.route("/api/v1.0/<start>/<end>")
+def tempByStartEnd(start,end):
+    session = Session(engine)
+
+    values = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) >= dt.datetime.strptime(start,"%Y-%m-%d")).\
+        filter(func.strftime("%Y-%m-%d", Measurement.date) <= dt.datetime.strptime(end,"%Y-%m-%d")).\
+        all()
+
+    session.close()
+
+    # Create dictionary
+    result = []
+    for mn, avg, mx in values:
+        tobs_stats_dict = {}
+        tobs_stats_dict["min"] = mn
+        tobs_stats_dict["avg"] = avg
+        tobs_stats_dict["max"] = mx
+        result.append(tobs_stats_dict)
+
+    return jsonify(result)
+
+
+
+############################################
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
